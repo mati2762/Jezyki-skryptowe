c30eb185f1edfe137aadae6170f86e37ac7f9157
Mati-Laptop <mati2762@gmail.com>
2021-04-18 12:56:45 +0200

    Add game pause

--- pygame-arkanoid/arkandoid.py
+++ pygame-arkanoid/arkandoid.py
@@ -3,7 +3,7 @@ from GameObjects.Player import Player
 from GameObjects.Ball import Ball
 from GameObjects.LevelGenerator import LevelGenerator
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from pygame.locals import KEYDOWN, K_ESCAPE, K_UP, K_DOWN, K_LEFT, K_RIGHT, QUIT, K_SPACE
+from pygame.locals import KEYDOWN, K_ESCAPE, K_UP, K_DOWN, K_LEFT, K_RIGHT, QUIT, K_SPACE, K_p
 
 pygame.init()
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
@@ -15,14 +15,15 @@ score = 0
 lives = 3
 pygame.display.set_caption("Arkanoid UJ")
 
-player = Player(SCREEN_WIDTH/2 , 450)
-ball = Ball(SCREEN_WIDTH/2 , 440)
+player = Player(SCREEN_WIDTH/2 - 45 , 450)
+ball = Ball(SCREEN_WIDTH/2  , 435)
 
 klocki = LevelGenerator.generate_level()
 
 pygame.mixer.music.load("sounds/intro.mp3")
 pygame.mixer.music.set_volume(0.2)
 pygame.mixer.music.play(-1)
+pause = False
 
 while uruchomiona:
 
@@ -35,6 +36,18 @@ while uruchomiona:
                 uruchomiona = False
             if event.key == K_SPACE:
                 ball.run_ball()
+            if event.key == pygame.K_p and ball.start == True:
+                    pause = not pause
+
+    # Pauza w grze
+    if pause :
+        font = pygame.font.Font(None, 44)
+        text = font.render("Game Paused", 1, (255, 255, 255))
+        screen.blit(text, ((SCREEN_WIDTH / 2) - 100 , SCREEN_HEIGHT / 2))
+        clock.tick(60)
+        pygame.display.flip()
+        continue;
+
 
     # Aktualizaja obiektów
     pressed_keys = pygame.key.get_pressed()
@@ -60,12 +73,11 @@ while uruchomiona:
     if pygame.sprite.collide_rect(ball, player):
         ball.velocity[0] = -ball.velocity[0]
         ball.bounce()
-        print("kolizja")
 
     # Restart Gry
     if ball.rect.y > SCREEN_HEIGHT - ball.height:
-        ball = Ball(SCREEN_WIDTH/2 + 20, 440)
-        player = Player(305, 450)
+        player = Player(SCREEN_WIDTH / 2 - 45, 450)
+        ball = Ball(SCREEN_WIDTH / 2, 435)
         lives -=1
         score = 0
         klocki = LevelGenerator.generate_level()
@@ -77,6 +89,11 @@ while uruchomiona:
     screen.blit(text, (10, SCREEN_HEIGHT - 20))
     text = font.render("Lives: " + str(lives), 1, (255, 255, 255))
     screen.blit(text, (SCREEN_WIDTH - 70 , SCREEN_HEIGHT - 20))
+
+    if ball.start == False:
+        font = pygame.font.Font(None, 44)
+        text = font.render("To start click [Space]" , 1, (255, 255, 255))
+        screen.blit(text, ((SCREEN_WIDTH / 2) - 160 , (SCREEN_HEIGHT / 2) + 70))
 
     clock.tick(60)
     pygame.display.flip()
