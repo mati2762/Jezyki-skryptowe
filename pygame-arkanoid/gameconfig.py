ebe93237eef9eeece106e7b8b815c254490a12d4
Mati-Laptop <mati2762@gmail.com>
2021-04-19 19:52:39 +0200

    Fix add the same score

--- pygame-arkanoid/gameconfig.py
+++ pygame-arkanoid/gameconfig.py
@@ -5,10 +5,18 @@ class GameConfig:
     bestScores = []
 
     @staticmethod
-    def save_score(score):
-        GameConfig.bestScores.append(score)
-        GameConfig.bestScores.sort(reverse=True)
-        file = open("bestScores.txt", "w")
-        for score in GameConfig.bestScores:
-            file.write(str(score) + "\n")
-        file.close()
+    def save_score(appScore):
+        found = False
+        try:
+            GameConfig.bestScores.index(appScore)
+            found = True
+        except:
+            found = False
+
+        if (found == False):
+            GameConfig.bestScores.append(appScore)
+            GameConfig.bestScores.sort(reverse=True)
+            file = open("bestScores.txt", "w")
+            for score in GameConfig.bestScores:
+                file.write(str(score) + "\n")
+            file.close()
